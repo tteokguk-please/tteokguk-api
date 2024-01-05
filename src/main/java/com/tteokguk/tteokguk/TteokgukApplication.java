faca3d823477bad31417b3727248442561678045
Haebin <joker7011@naver.com>
2024-01-05 23:50:37 +0900

    feat: Initial Commit

---
+++ src/main/java/com/tteokguk/tteokguk/TteokgukApplication.java
@@ -0,0 +1,13 @@
+package com.tteokguk.tteokguk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TteokgukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TteokgukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tteokguk/tteokguk/TteokgukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tteokguk.tteokguk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TteokgukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
