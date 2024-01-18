7fa1bdf6432511a18d43fa07ccc8fec3e3ef2352
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-19 01:37:08 +0900

    docs: TODO 주석 추가

--- src/main/java/com/tteokguk/tteokguk/member/domain/SimpleMember.java
+++ src/main/java/com/tteokguk/tteokguk/member/domain/SimpleMember.java
@@ -38,6 +38,7 @@ public class SimpleMember extends Member {
 	}
 
 	// TODO: 전용 재료를 회원 가입 때 정하는 로직을 구현해야 한다.
+	// TODO: Password Encoder를 통해 비밀번호를 암호화해야 한다.
 	public static SimpleMember join(String email, String password, String nickname) {
 		return new SimpleMember(email, password, nickname, Ingredient.BEEF, new ArrayList<>());
 	}
