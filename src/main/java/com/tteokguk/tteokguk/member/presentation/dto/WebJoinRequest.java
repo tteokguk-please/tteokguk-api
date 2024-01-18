7f6df4618cacc6fbb8cb569b2c89c514e8e6c6b3
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-19 01:38:09 +0900

    docs: TODO 주석 추가

--- src/main/java/com/tteokguk/tteokguk/member/presentation/dto/WebJoinRequest.java
+++ src/main/java/com/tteokguk/tteokguk/member/presentation/dto/WebJoinRequest.java
@@ -2,6 +2,7 @@ package com.tteokguk.tteokguk.member.presentation.dto;
 
 import com.tteokguk.tteokguk.member.application.dto.AppJoinRequest;
 
+// TODO: 요청 바디 검증이 필요하다.
 public record WebJoinRequest(
 	String email,
 	String password,
