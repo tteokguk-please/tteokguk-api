c9064423eea855bbb9baba984d18f15a36cce403
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-20 22:50:28 +0900

    feat: 이메일 인증 기능 추가

--- src/main/java/com/tteokguk/tteokguk/global/security/SecurityConfig.java
+++ src/main/java/com/tteokguk/tteokguk/global/security/SecurityConfig.java
@@ -1,28 +1,85 @@
 package com.tteokguk.tteokguk.global.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.tteokguk.tteokguk.global.security.filter.CustomAuthenticationFilter;
+import com.tteokguk.tteokguk.global.security.model.PrincipalDetailsService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final PrincipalDetailsService principalDetailsService;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 			.csrf(AbstractHttpConfigurer::disable)
+			.cors(configurer -> corsConfigurationSource())
 			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
 			.build();
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public CustomAuthenticationFilter customAuthenticationFilter() {
+		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter();
+		customAuthenticationFilter.setFilterProcessesUrl("/api/v1/auth/login");
+		customAuthenticationFilter.setAuthenticationManager(
+			authenticationManager(passwordEncoder())
+		);
+		return customAuthenticationFilter;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+		PasswordEncoder passwordEncoder
+	) {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(principalDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+		ProviderManager providerManager = new ProviderManager(authenticationProvider);
+		providerManager.setEraseCredentialsAfterAuthentication(false);
+
+		return providerManager;
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOriginPatterns(List.of("*"));
+		configuration.setAllowedMethods(List.of("HEAD", "POST", "GET", "DELETE", "PUT"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
 	}
 }
---
+++ src/main/java/com/tteokguk/tteokguk/global/security/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.tteokguk.tteokguk.global.security.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	record LoginRequest(
+		String email,
+		String password
+	) {}
+
+	private ObjectMapper om = new ObjectMapper();
+
+	@Override
+	public Authentication attemptAuthentication(
+		HttpServletRequest request,
+		HttpServletResponse response
+	) {
+		UsernamePasswordAuthenticationToken authenticationToken = getAuthenticationToken(request);
+		setDetails(request, authenticationToken);
+		Authentication authenticate;
+		try {
+			authenticate = getAuthenticationManager().authenticate(authenticationToken);
+		} catch (AuthenticationException e) {
+			request.setAttribute("exception", e);
+			throw e;
+		}
+		return authenticate;
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthenticationToken(HttpServletRequest request) {
+		try {
+			LoginRequest dto = om.readValue(request.getInputStream(), LoginRequest.class);
+			log.debug("CustomAuthenticationFilter :: email : {}, password : {}", dto.email, dto.password);
+			return new UsernamePasswordAuthenticationToken(dto.email, dto.password);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
