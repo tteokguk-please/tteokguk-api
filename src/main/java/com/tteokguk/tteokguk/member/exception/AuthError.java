7cc7392426b03ff596e50c73b29bc2dcdac2a768
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-21 12:54:02 +0900

    feat: JWT 추가

--- src/main/java/com/tteokguk/tteokguk/member/exception/AuthError.java
+++ src/main/java/com/tteokguk/tteokguk/member/exception/AuthError.java
@@ -15,6 +15,10 @@ public enum AuthError implements ErrorCode {
 	DUPLICATE_NICKNAME("닉네임이 중복되었습니다.", HttpStatus.BAD_REQUEST, "A002"),
 	BAD_EMAIL("잘못된 이메일입니다.", HttpStatus.UNAUTHORIZED, "A003"),
 	BAD_PASSWORD("잘못된 비밀번호입니다.", HttpStatus.UNAUTHORIZED, "A004"),
+	INVALID_JWT_SIGNATURE("시그니처가 유효하지 않습니다.", HttpStatus.UNAUTHORIZED, "A005"),
+	INVALID_JWT_TOKEN("토큰이 유효하지 않습니다.", HttpStatus.UNAUTHORIZED, "A006"),
+	EXPIRED_JWT_TOKEN("토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED, "A007"),
+	UNSUPPORTED_JWT_TOKEN("지원되지 않는 토큰입니다.", HttpStatus.UNAUTHORIZED, "A008"),
 	;
 
 	private final String message;
