a63ce6d6235d6acfcd237a36757e8bd76851fcbe
Haebin <joker7011@naver.com>
2024-01-25 01:20:23 +0900

    chore: 불필요 어노테이션 제거

--- src/main/java/com/tteokguk/tteokguk/member/application/ItemService.java
+++ src/main/java/com/tteokguk/tteokguk/member/application/ItemService.java
@@ -1,42 +1,38 @@
 package com.tteokguk.tteokguk.member.application;
 
-import static lombok.AccessLevel.*;
+import com.tteokguk.tteokguk.member.domain.Item;
+import com.tteokguk.tteokguk.tteokguk.constants.Ingredient;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.tteokguk.tteokguk.member.domain.Item;
-import com.tteokguk.tteokguk.tteokguk.constants.Ingredient;
-
-import lombok.NoArgsConstructor;
+import static lombok.AccessLevel.PROTECTED;
 
 @Component
-@Transactional
 @NoArgsConstructor(access = PROTECTED)
 public class ItemService {
 
-	public static boolean hasSufficientIngredients(
-		List<Item> items,
-		List<Ingredient> ingredients
-	) {
-		return ingredients.stream()
-			.allMatch(
-				ingredient -> items.stream()
-					.filter(item -> item.getIngredient().equals(ingredient))
-					.allMatch(item -> item.getStockQuantity() >= 1)
-			);
-	}
+    public static boolean hasSufficientIngredients(
+            List<Item> items,
+            List<Ingredient> ingredients
+    ) {
+        return ingredients.stream()
+                .allMatch(
+                        ingredient -> items.stream()
+                                .filter(item -> item.getIngredient().equals(ingredient))
+                                .allMatch(item -> item.getStockQuantity() >= 1)
+                );
+    }
 
-	public static void decreaseStockQuantities(
-		List<Item> items,
-		List<Ingredient> ingredients
-	) {
-		ingredients.forEach(ingredient -> {
-			items.stream()
-				.filter(item -> item.getIngredient().equals(ingredient))
-				.forEach(item -> item.decreaseStockQuantity(1));
-		});
-	}
+    public static void decreaseStockQuantities(
+            List<Item> items,
+            List<Ingredient> ingredients
+    ) {
+        ingredients.forEach(ingredient -> {
+            items.stream()
+                    .filter(item -> item.getIngredient().equals(ingredient))
+                    .forEach(item -> item.decreaseStockQuantity(1));
+        });
+    }
 }
--- src/main/java/com/tteokguk/tteokguk/member/application/MyPageService.java
+++ src/main/java/com/tteokguk/tteokguk/member/application/MyPageService.java
@@ -2,17 +2,15 @@ package com.tteokguk.tteokguk.member.application;
 
 import com.tteokguk.tteokguk.global.exception.BusinessException;
 import com.tteokguk.tteokguk.member.application.dto.response.MyPageResponse;
-import com.tteokguk.tteokguk.member.application.dto.response.MyPageResponseAssembler;
+import com.tteokguk.tteokguk.member.application.dto.response.assembler.MyPageResponseAssembler;
 import com.tteokguk.tteokguk.member.domain.Member;
 import com.tteokguk.tteokguk.member.infra.persistence.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.tteokguk.tteokguk.member.exception.MemberError.MEMBER_NOT_FOUND;
 
-@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
