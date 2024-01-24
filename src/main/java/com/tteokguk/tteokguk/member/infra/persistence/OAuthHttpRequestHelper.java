88d50a40e09904949c0ee16b638edd3188a6a1e5
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-24 21:02:05 +0900

    feat: 접근 토큰 획득 로직 추가

--- src/main/java/com/tteokguk/tteokguk/member/infra/persistence/OAuthHttpRequestHelper.java
+++ src/main/java/com/tteokguk/tteokguk/member/infra/persistence/OAuthHttpRequestHelper.java
@@ -1,9 +1,19 @@
 package com.tteokguk.tteokguk.member.infra.persistence;
 
+import static com.tteokguk.tteokguk.member.application.properties.OAuthProperties.*;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import com.tteokguk.tteokguk.member.application.properties.OAuthProperties;
+import com.tteokguk.tteokguk.member.domain.ProviderType;
+import com.tteokguk.tteokguk.tteokguk.infra.persistence.dto.TokenResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,4 +30,27 @@ public class OAuthHttpRequestHelper {
 	private final RestTemplate restTemplate;
 	private final OAuthProperties oAuthProperties;
 
+	public TokenResponse exchangeToken(ProviderType providerType, String code) {
+		OAuthProviderProperty providerProperty = oAuthProperties.getProviderProperties(providerType);
+		OAuthRegistrationProperty registrationProperty = oAuthProperties.getRegistrationProperties(providerType);
+
+		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add(GRANT_TYPE, registrationProperty.authorizationGrantType());
+		params.add(CODE, code);
+		params.add(REDIRECT_URI, registrationProperty.redirectUri());
+		params.add(CLIENT_ID, registrationProperty.clientId());
+		params.add(CLIENT_SECRET, registrationProperty.clientSecret());
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers.add("Accept", "application/json");
+
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+		ResponseEntity<TokenResponse> response = restTemplate.postForEntity(
+			providerProperty.tokenUri(), request, TokenResponse.class
+		);
+
+		return response.getBody();
+	}
 }
