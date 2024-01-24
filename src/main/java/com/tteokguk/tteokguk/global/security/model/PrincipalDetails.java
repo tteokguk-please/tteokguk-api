9557c276093eccf70a1cb0b9defc0d6f9b4c8fbf
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-25 01:19:31 +0900

    style: 불필요한 import 제거

--- src/main/java/com/tteokguk/tteokguk/global/security/model/PrincipalDetails.java
+++ src/main/java/com/tteokguk/tteokguk/global/security/model/PrincipalDetails.java
@@ -2,7 +2,6 @@ package com.tteokguk.tteokguk.global.security.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
