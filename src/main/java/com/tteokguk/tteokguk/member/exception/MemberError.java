17b6ecda4dc28d55339a75b6b8dc83848208be0a
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-26 03:13:20 +0900

    refactor: 이메일 중복, 닉네임 중복을 MemberError로 이동

--- src/main/java/com/tteokguk/tteokguk/member/exception/MemberError.java
+++ src/main/java/com/tteokguk/tteokguk/member/exception/MemberError.java
@@ -15,6 +15,8 @@ public enum MemberError implements ErrorCode {
 
 	MEMBER_NOT_FOUND("해당 사용자를 찾을 수 없습니다.", NOT_FOUND, "M_001"),
 	NOT_TEMP_USER("해당 사용자는 임시 유저가 아닙니다.", BAD_REQUEST, "M_002"),
+	DUPLICATE_EMAIL("이메일이 중복되었습니다.", BAD_REQUEST, "M_003"),
+	DUPLICATE_NICKNAME("닉네임이 중복되었습니다.", BAD_REQUEST, "M_004"),
 	;
 
 	private final String message;
