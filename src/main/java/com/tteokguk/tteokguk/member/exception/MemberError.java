a559375088a2868fe5d00285d88482220ea0eb07
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-26 02:53:14 +0900

    feat: 임시 유저가 아닐 시 에러 반환을 위한 enum 추가

--- src/main/java/com/tteokguk/tteokguk/member/exception/MemberError.java
+++ src/main/java/com/tteokguk/tteokguk/member/exception/MemberError.java
@@ -13,7 +13,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MemberError implements ErrorCode {
 
-	MEMBER_NOT_FOUND("해당 사용자를 찾을 수 없습니다.", NOT_FOUND, "M_001");
+	MEMBER_NOT_FOUND("해당 사용자를 찾을 수 없습니다.", NOT_FOUND, "M_001"),
+	NOT_TEMP_USER("해당 사용자는 임시 유저가 아닙니다.", BAD_REQUEST, "M_002"),
+	;
 
 	private final String message;
 	private final HttpStatus status;
