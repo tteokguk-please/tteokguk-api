0621b6e8ac1cc3da0eac1a7814360eb0358f664a
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-26 03:13:50 +0900

    feat: 닉네임 중복 시 예외 응답 반환 기능 생성

--- src/main/java/com/tteokguk/tteokguk/member/application/UserInfoService.java
+++ src/main/java/com/tteokguk/tteokguk/member/application/UserInfoService.java
@@ -6,6 +6,8 @@ import com.tteokguk.tteokguk.member.application.dto.response.MyPageResponse;
 import com.tteokguk.tteokguk.member.application.dto.response.UserInfoResponse;
 import com.tteokguk.tteokguk.member.application.dto.response.assembler.UserInfoResponseAssembler;
 import com.tteokguk.tteokguk.member.domain.Member;
+import com.tteokguk.tteokguk.member.exception.AuthError;
+import com.tteokguk.tteokguk.member.exception.MemberError;
 import com.tteokguk.tteokguk.member.infra.persistence.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -35,6 +37,9 @@ public class UserInfoService {
     }
 
     public void initialize(Long memberId, AppInitRequest request) {
+        if (memberRepository.existsByNickname(request.nickname()))
+            throw new BusinessException(MemberError.DUPLICATE_EMAIL);
+
         Member member = memberRepository.findById(memberId)
             .orElseThrow(() -> BusinessException.of(MEMBER_NOT_FOUND));
 
