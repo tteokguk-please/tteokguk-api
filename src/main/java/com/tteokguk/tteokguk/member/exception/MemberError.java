24f169683dc019dc5320d850664c0049a8ad7e6f
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-26 03:36:07 +0900

    feat: 사용하지 않는 enum 제거

--- src/main/java/com/tteokguk/tteokguk/member/exception/MemberError.java
+++ src/main/java/com/tteokguk/tteokguk/member/exception/MemberError.java
@@ -14,9 +14,8 @@ import lombok.RequiredArgsConstructor;
 public enum MemberError implements ErrorCode {
 
 	MEMBER_NOT_FOUND("해당 사용자를 찾을 수 없습니다.", NOT_FOUND, "M_001"),
-	NOT_TEMP_USER("해당 사용자는 임시 유저가 아닙니다.", BAD_REQUEST, "M_002"),
-	DUPLICATE_EMAIL("이메일이 중복되었습니다.", BAD_REQUEST, "M_003"),
-	DUPLICATE_NICKNAME("닉네임이 중복되었습니다.", BAD_REQUEST, "M_004"),
+	DUPLICATE_EMAIL("이메일이 중복되었습니다.", BAD_REQUEST, "M_002"),
+	DUPLICATE_NICKNAME("닉네임이 중복되었습니다.", BAD_REQUEST, "M_003"),
 	;
 
 	private final String message;
