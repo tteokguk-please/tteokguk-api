597f87558c55415866bad1e560689017242188ab
Myeongbeom Lee <mungmnb777@gmail.com>
2024-01-26 21:05:50 +0900

    feat: JwtService 생성

---
+++ src/main/java/com/tteokguk/tteokguk/global/security/jwt/JwtService.java
@@ -0,0 +1,42 @@
+package com.tteokguk.tteokguk.global.security.jwt;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.tteokguk.tteokguk.global.utils.LocalDateTimeUtils;
+import com.tteokguk.tteokguk.member.domain.Member;
+import com.tteokguk.tteokguk.member.domain.RefreshToken;
+import com.tteokguk.tteokguk.member.infra.persistence.RefreshTokenRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class JwtService {
+
+	private final JwtFactory jwtFactory;
+	private final RefreshTokenRepository refreshTokenRepository;
+
+	public Jwt getAccessToken(Member member, Long now) {
+		return jwtFactory.createAuthToken(
+			String.valueOf(member.getId()), member.getRole().name(), new Date(jwtFactory.getExpiryOfAccessToken(now))
+		);
+	}
+
+	public Jwt getRefreshToken(Member member, Long now) {
+		Long expiry = jwtFactory.getExpiryOfRefreshToken(now);
+		Jwt refreshToken = jwtFactory.createAuthToken(null, new Date(expiry));
+		String encodedBody = refreshToken.getEncodedBody();
+		LocalDateTime expiryDateTime = LocalDateTimeUtils.convertBy(expiry);
+
+		RefreshToken entity = refreshTokenRepository.findByMember(member)
+			.orElseGet(() -> new RefreshToken(member, encodedBody, expiryDateTime));
+		entity.update(encodedBody, expiryDateTime);
+		refreshTokenRepository.save(entity);
+		return refreshToken;
+	}
+}
