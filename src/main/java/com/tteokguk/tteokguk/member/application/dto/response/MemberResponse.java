10dfc7f4e3dcbfebe4b77ff48c08ca38da6bf393
이명범 <72181693+mungmnb777@users.noreply.github.com>
2024-02-02 14:07:56 +0900

    fix: 유저 검색 API의 응답값 변경 (#75)

--- src/main/java/com/tteokguk/tteokguk/member/application/dto/response/MemberResponse.java
+++ src/main/java/com/tteokguk/tteokguk/member/application/dto/response/MemberResponse.java
@@ -1,14 +1,7 @@
 package com.tteokguk.tteokguk.member.application.dto.response;
 
-import java.util.List;
-
-import com.tteokguk.tteokguk.tteokguk.application.dto.response.SimpleTteokgukResponse;
-import com.tteokguk.tteokguk.tteokguk.constants.Ingredient;
-
 public record MemberResponse(
 	Long id,
-	String nickname,
-	Ingredient primaryIngredient,
-	List<SimpleTteokgukResponse> tteokguks
+	String nickname
 ) {
 }
