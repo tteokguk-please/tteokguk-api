dac8e050afa75f197e470acec307e99d2347dd0d
이명범 <72181693+mungmnb777@users.noreply.github.com>
2024-02-03 16:35:37 +0900

    feat: 카카오 로그인, 일반 로그인, 토큰 갱신 시 닉네임, 전용 재료, 유저의 재료 재고를 응답에 추가 (#81)
    * feat: 카카오 로그인 시 닉네임, 전용 재료, 재료 재고를 응답에 포함
    
    * feat: 일반 로그인 시 닉네임, 전용 재료, 재료 재고를 응답에 포함
    
    * feat: 토큰 갱신 시 닉네임, 전용 재료, 재료 재고를 응답에 포함

--- src/main/java/com/tteokguk/tteokguk/global/security/dto/WebLoginResponse.java
+++ src/main/java/com/tteokguk/tteokguk/global/security/dto/WebLoginResponse.java
@@ -1,7 +1,15 @@
 package com.tteokguk.tteokguk.global.security.dto;
 
+import java.util.List;
+
+import com.tteokguk.tteokguk.item.application.dto.response.ItemResponse;
+import com.tteokguk.tteokguk.tteokguk.constants.Ingredient;
+
 public record WebLoginResponse(
 	Long id,
+	String nickname,
+	Ingredient primaryIngridient,
+	List<ItemResponse> items,
 	String accessToken,
 	String refreshToken
 ) {
--- src/main/java/com/tteokguk/tteokguk/member/presentation/dto/WebIssuedTokensResponse.java
+++ src/main/java/com/tteokguk/tteokguk/member/presentation/dto/WebIssuedTokensResponse.java
@@ -1,13 +1,28 @@
 package com.tteokguk.tteokguk.member.presentation.dto;
 
+import java.util.List;
+
+import com.tteokguk.tteokguk.item.application.dto.response.ItemResponse;
 import com.tteokguk.tteokguk.member.application.dto.response.AppIssuedTokensResponse;
+import com.tteokguk.tteokguk.member.application.dto.response.MyPageResponse;
+import com.tteokguk.tteokguk.tteokguk.constants.Ingredient;
 
 public record WebIssuedTokensResponse(
 	Long id,
+	String nickname,
+	Ingredient primaryIngredient,
+	List<ItemResponse> items,
 	String accessToken,
 	String refreshToken
 ) {
-	public static WebIssuedTokensResponse of(AppIssuedTokensResponse response) {
-		return new WebIssuedTokensResponse(response.id(), response.accessToken(), response.refreshToken());
+	public static WebIssuedTokensResponse of(AppIssuedTokensResponse response, MyPageResponse myInfo) {
+		return new WebIssuedTokensResponse(
+			response.id(),
+			myInfo.nickname(),
+			myInfo.primaryIngredient(),
+			myInfo.items(),
+			response.accessToken(),
+			response.refreshToken()
+		);
 	}
 }
