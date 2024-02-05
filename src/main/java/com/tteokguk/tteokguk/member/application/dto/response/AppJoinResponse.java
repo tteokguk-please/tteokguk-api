efb221f59ea56e612767d9dc36f35331e0520559
Myeongbeom Lee <mungmnb777@gmail.com>
2024-02-06 02:04:34 +0900

    feat: 회원가입 성공 시 접근 토큰, 갱신 토큰도 반환

--- src/main/java/com/tteokguk/tteokguk/member/application/dto/response/AppJoinResponse.java
+++ src/main/java/com/tteokguk/tteokguk/member/application/dto/response/AppJoinResponse.java
@@ -5,13 +5,17 @@ import com.tteokguk.tteokguk.member.domain.Member;
 public record AppJoinResponse(
 	Long id,
 	String nickname,
-	String primaryIngredient
+	String primaryIngredient,
+	String accessToken,
+	String refreshToken
 ) {
-	public static AppJoinResponse of(Member entity) {
+	public static AppJoinResponse of(Member entity, String accessToken, String refreshToken) {
 		return new AppJoinResponse(
 			entity.getId(),
 			entity.getNickname(),
-			entity.getPrimaryIngredient().name()
+			entity.getPrimaryIngredient().name(),
+			accessToken,
+			refreshToken
 		);
 	}
 }
